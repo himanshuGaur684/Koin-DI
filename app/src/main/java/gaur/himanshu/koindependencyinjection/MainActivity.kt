d8d6c03faaf6cad8f842e0c0cb5eb820cd6c3624
Himanshu <himanshugaur684@gmail.com>
2024-12-08 19:53:23 +0530

    Initial Project for Koin Annotations.

--- app/src/main/java/gaur/himanshu/koindependencyinjection/MainActivity.kt
+++ app/src/main/java/gaur/himanshu/koindependencyinjection/MainActivity.kt
@@ -4,93 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import gaur.himanshu.koindependencyinjection.ui.theme.KoinDependencyInjectionTheme
-import gaur.himanshu.koindependencyinjection.view.AuthViewModel
-import gaur.himanshu.koindependencyinjection.view.SessionManager
-import gaur.himanshu.koindependencyinjection.view.User
-import gaur.himanshu.koindependencyinjection.view.UserScope
-import org.koin.android.ext.android.getKoin
-import org.koin.android.ext.android.inject
-import org.koin.android.scope.AndroidScopeComponent
-import org.koin.androidx.scope.activityScope
-import org.koin.androidx.viewmodel.ext.android.getViewModel
-import org.koin.core.qualifier.named
-import org.koin.core.scope.Scope
 
-class MainActivity : ComponentActivity(), AndroidScopeComponent {
-
-    override val scope: Scope by activityScope()
-
-    private val sessionManager: SessionManager by scope.inject()
-
-    private val first: String by inject(named("first"))
-    private val second: String by inject(named("second"))
-
-    private lateinit var userScope: Scope
-    private lateinit var user: User
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        userScope = getKoin().createScope("scope_id", UserScope)
-        user = userScope.get()
-
 
         enableEdgeToEdge()
         setContent {
             KoinDependencyInjectionTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
-                    val viewModel: AuthViewModel = getViewModel()
-                    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
-                    if (uiState.isLoading) {
-                        Box(
-                            Modifier
-                                .padding(innerPadding)
-                                .fillMaxSize(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            CircularProgressIndicator()
-                        }
-                    }
-
-                    if (uiState.error.isNotBlank()) {
-                        Box(
-                            Modifier
-                                .padding(innerPadding)
-                                .fillMaxSize(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(uiState.error)
-                        }
-                    }
-
-                    uiState.data?.let {
-                        Greeting(
-                            name = it.plus(first).plus(second),
-                            modifier = Modifier.padding(innerPadding)
-                        )
-                    }
                 }
             }
         }
     }
 
     override fun onDestroy() {
-        userScope.close()
         super.onDestroy()
     }
 }
--- app/src/main/java/gaur/himanshu/koindependencyinjection/domain/DomainModule.kt
+++
@@ -1,8 +0,0 @@
-package gaur.himanshu.koindependencyinjection.domain
-
-import gaur.himanshu.koindependencyinjection.domain.use_cases.GetUserNameUseCase
-import org.koin.dsl.module
-
-val domainModule = module {
-    factory { GetUserNameUseCase(get()) }
-}
